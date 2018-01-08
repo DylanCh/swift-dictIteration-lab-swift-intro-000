d085a25e50b7a3eae3ccafe0c23cc4b39729cd41
Hanjun Chen <hchen004@citymail.cuny.edu>
2018-01-08 17:13:51 -0500

    Update DictionaryIteration.swift

--- DictionaryIteration/DictionaryIteration.swift
+++ DictionaryIteration/DictionaryIteration.swift
@@ -11,18 +11,27 @@ import Foundation
 class DictionaryIteration {
     
     // Replace this comment with your answer to Question #1.
-
+    var bands:[String:String] = [
+        "Nirvana" : "Nevermind",
+        "Blondie" : "Parallel Lines",
+        "The Kinks" : "Low Budget",
+        "The Beatles" : "Sgt. Pepper's Lonely Hearts Club Band"
+    ]
     
     
     
     // Question #2
     
-    
+    func addPixie(){
+        bands["Pixie"] = "Doolittle"
+    }
     
     
     
     // Question #3
-   
+   func removeKinks(){
+       bands["The Kinks"] = nil
+   }
     
     
     
@@ -31,25 +40,36 @@ class DictionaryIteration {
         let bandName = "Nirvana"
       
         // remove this return statement and replace it with what should be returned when you complete your implementation.
-        return ""
+        return String(format:"%@'s top-selling album was %@",bandName,bands[bandName])
     }
     
     
     // Question #5
    
-    
+    func bestSellers() -> String {
+        var  sentence = ""
+        bands.forEach{ sentence += (String(format:"%@'s top-selling album was %@\n",$0,$1) }
+        return sentence
+    }
     
     
     
     // Question #6
-   
+   func typeOfDataStructure() -> String{
+       for (key,val) in bands {
+           return type(of: val)
+       }
+   }
     
     
     
     
     // Question #7
    
-    
+    func typeOfDataStructureForSchool() -> String{
+        // let arr = ["A","B","C","D"]
+        // return arr[Int(arc4random_uniform(UInt32(arr.count)))]
+    }
     
     
     
